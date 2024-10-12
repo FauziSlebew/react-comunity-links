c077695bd641f1d9bf5a41af3dff53568f8405d8
Fauzii <113253689+FauziSlebew@users.noreply.github.com>
2024-10-12 23:52:58 +0800

    Update App.js

--- src/App.js
+++ src/App.js
@@ -10,24 +10,24 @@ function App() {
         <meta name="viewport" content="width=device-width" />
         <meta property="og:title" content="Dikira Kira Saja" />
         <meta property="og:image"
-          content="https://f.balxzzy.xyz/component/IMG-20240928-WA0193.jpg"/>
+          content="https://files.catbox.moe/6mz5yr.jpg"/>
         <meta property="og:description"
           content="WhatsApp bot community, WhatsApp bot dev, Community WhatsApp bot, WhatsApp bot drv, wabot"/>
         <meta name="twitter:image:src"
-          content="https://f.balxzzy.xyz/component/IMG-20240928-WA0193.jpg"/>
+          content="https://files.catbox.moe/6mz5yr.jpg"/>
         <title>Community</title>
       </Helmet>
       
       <div className="container">
         <img alt="profile picture" className="profile-pic"
-          src="https://f.balxzzy.xyz/component/IMG-20240928-WA0193.jpg"/>
+          src="https://files.catbox.moe/6mz5yr.jpg"/>
         <div className="title">
           Xshiina - Community <i className="fas fa-check-circle"></i>
         </div>
         
         <div className="subtitle">Beginner Team, Suka Bikin Honar</div>
         <div className="social-icons">
-          <a href="https://instagram.com/@iqstore78">
+          <a href="https://instagram.com/axlfauzixy">
             <i className="fab fa-instagram"></i>
           </a>
           
@@ -39,13 +39,13 @@ function App() {
         <Section title="Social Dev Link">
           <LinkItem href="https://me.balxzzy.xyz" icon="fa-brands fa-edge" text="My Portfolio"/>
           <LinkItem href="https://api.balxzzy.xyz" icon="fa-regular fa-fire" text="Projects Rest API's"/>
-          <LinkItem href="https://github.com/balxz" icon="fa-brands fa-github" text="Github"/>
-          <LinkItem href="https://tiktok.com/@iqstore78" icon="fa-brands fa-tiktok" text="Tiktok"/>
+          <LinkItem href="https://github.com/FauziSlebew" icon="fa-brands fa-github" text="Github"/>
+          <LinkItem href="https://tiktok.com/@zyziizi" icon="fa-brands fa-tiktok" text="Tiktok"/>
         </Section>
 
         <Section title="Community - Codings">
-          <LinkItem href="https://chat.whatsapp.com/FW78A12iQIdK3snmkSKzQ3" icon="fab fa-whatsapp" text="Shiina Wabot Groups"/>
-          <LinkItem href="https://whatsapp.com/channel/0029VaSY7Lp8F2pCmQLKNn0g" icon="fab fa-whatsapp" text="Shiina Channels Community"/>
+          <LinkItem href="https://chat.whatsapp.com/DJ3fUwY23dG3smRmSIg3uK" icon="fab fa-whatsapp" text="Group Emu"/>
+          <LinkItem href="https://whatsapp.com/channel/0029ValsXDeEKyZEtDz3A20L" icon="fab fa-whatsapp" text="Emu Channels Community"/>
           <LinkItem href="https://hello.com" icon="fab fa-whatsapp" text="Gmn Team"/>
           <LinkItem href="https://chat.whatsapp.com/JbzMsezhCwUKdC6dnjwcIz" icon="fab fa-whatsapp" text="All Bot Community"/>
           <LinkItem href="https://chat.whatsapp.com/ExSzRGszuh2FIiMctAQJMZ" icon="fab fa-whatsapp" text="BinaryCrafters"/>
